ee30f1e58432c6d2c41cb7d4d291884fa5cd6a20
Prince <prince2520p@gmail.com>
2023-08-13 21:49:56 +0530

    added auto logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,36 @@ import './App.css';
 import Chat from "./Pages/Chat/Chat";
 import {Navigate, Route, Routes} from "react-router-dom";
 import AuthContext from "./Context/auth";
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import Authentication from "./Pages/Authentication/Authentication";
 import Login from "./Pages/Authentication/LoginSignUp/Login/Login";
 import SignUp from "./Pages/Authentication/LoginSignUp/SignUp/SignUp";
-import GroupList from "./Pages/Chat/ChatTab/GroupPrivateList/GroupList/GroupList";
 import EditProfile from "./Pages/Chat/ChatTab/EditProfile/EditProfile";
 import JoinGroup from "./Pages/Chat/ChatTab/JoinGroup/JoinGroup";
 import CreateGroup from "./Pages/Chat/ChatTab/CreateGroup/CreateGroup";
 import AlertBox from "./Helper/AlertBox/AlertBox";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import PrivateList from "./Pages/Chat/ChatTab/GroupPrivateList/PrivateList/PrivateList";
 import GroupPrivateList from "./Pages/Chat/ChatTab/GroupPrivateList/GroupPrivateList";
+import {AlertBoxActions} from "./store/alert";
 
 
+let time = null;
 function App() {
     const authCtx = useContext(AuthContext);
+    const dispatch = useDispatch();
     const showAlertBox = useSelector(state=>state.alert.showAlertBox);
+    const showAlertMessage = useSelector(state=>state.alert.message);
+
+    useEffect(() => {
+        clearTimeout(time);
+        if (showAlertBox) {
+            time = setTimeout(() => {
+                dispatch(AlertBoxActions.closeAlertBoxHandler());
+            }, [3000]);
+        }
+    }, [dispatch, showAlertBox, showAlertMessage]);
+
 
     return (
         <div className="App">
