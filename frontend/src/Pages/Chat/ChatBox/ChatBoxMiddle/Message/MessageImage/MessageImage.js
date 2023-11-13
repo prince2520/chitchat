db52b41d61025f71ee25411318143ee870ef3f9a
Prince <prince2520p@gmail.com>
2023-11-13 17:38:53 +0530

    added on click large image display

--- frontend/src/Pages/Chat/ChatBox/ChatBoxMiddle/Message/MessageImage/MessageImage.js
+++ frontend/src/Pages/Chat/ChatBox/ChatBoxMiddle/Message/MessageImage/MessageImage.js
@@ -1,32 +1,26 @@
 import React, {useEffect, useState} from "react";
 import {Icon} from "@iconify/react";
 
+import ModalImage from "react-modal-image";
+
 import './MessageImage.css';
-import {useInView} from "react-intersection-observer";
-import {useStore} from "react-redux";
+
 
 const MessageImage = ({myMsg, imageSrc, time}) => {
 
-    const [blurImg, setBlurImg] = useState(false);
-
-    const {ref, inView} = useInView({
-        threshold: 0.1,
-        delay: 100,
-    });
-
-    useEffect(() => {
-        setBlurImg(!inView);
-    }, [inView]);
-
     return (
         <div
-            ref={ref}
             className={'send-img chat-msg-background media-container'}
             style={{
                 borderColor: myMsg && 'var(--white)',
                 alignSelf: !myMsg && 'flex-start'
             }}>
-            <img className={'cursor-btn'} alt={'send-message'} style={{filter: `blur(${blurImg? 5: 0 }px)`}} src={imageSrc}/>
+            <ModalImage
+                small={imageSrc}
+                medium={imageSrc}
+                hideDownload={true}
+                showRotate={true}
+            />
             <div className={'image-bottom'}>
                 <Icon icon="typcn:image"/>
                 <span className={'msg-time'}>{time}</span>
