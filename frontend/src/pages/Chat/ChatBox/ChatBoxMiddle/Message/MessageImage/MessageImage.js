06544ee252d6c1fae5227a5350bcb3fdbdf6a17d
Prince Kumar <prince2520p@gmail.com>
2024-04-05 15:30:19 +0530

    refactor: optimization in authentication

--- frontend/src/pages/Chat/ChatBox/ChatBoxMiddle/Message/MessageImage/MessageImage.js
+++ frontend/src/pages/Chat/ChatBox/ChatBoxMiddle/Message/MessageImage/MessageImage.js
@@ -13,6 +13,7 @@ const MessageImage = ({ myMsg, imageSrc, time }) => {
         alignSelf: !myMsg && "flex-start",
       }}
     >
+
       <ModalImage
         small={imageSrc}
         medium={imageSrc}
